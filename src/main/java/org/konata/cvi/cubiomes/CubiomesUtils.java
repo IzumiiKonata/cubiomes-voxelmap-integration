ed98eb0386f87c795240cd1732047f343e8e9a4d
IzumiiKonata <2030752250@qq.com>
2026-03-04 21:35:00 +0800

    Initial commit

---
+++ src/main/java/org/konata/cvi/cubiomes/CubiomesUtils.java
@@ -0,0 +1,11 @@
+package org.konata.cvi.cubiomes;
+
+/**
+ * @author IzumiiKonata
+ * Date: 2026/3/4 21:21
+ */
+public class CubiomesUtils {
+
+
+
+}
---
+++ src/main/java/org/konata/cvi/cubiomes/datatypes/Pos.java
@@ -0,0 +1,11 @@
+package org.konata.cvi.cubiomes.datatypes;
+
+import lombok.Data;
+
+/**
+ * @author IzumiiKonata
+ * Date: 2026/3/4 21:22
+ */
+@Data
+public class Pos {
+}
